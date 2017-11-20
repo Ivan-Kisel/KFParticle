cfce5379122a9be108d174aeb5af9580e7b7f89e
mzyzak <mzyzak>
2017-11-20 14:31:43 +0000

    Add CMake and Linkdef to compile the library.

---
+++ KFLinkDef.h
@@ -0,0 +1,19 @@
+#if defined(__CINT__) || defined(__CLING__)
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+//KFParticle
+#pragma link C++ class  KFParticleBase+;
+#pragma link C++ class  KFParticle+;
+#pragma link C++ class  KFVertex+;
+#pragma link C++ class  KFPartEfficiencies+;
+#pragma link C++ class  KFMCParticle+;
+
+#endif
+
+
+
+
+
