e6d5d9bd616c11e87e1eb2fbd346fbb85952b710
m.zyzak <m.zyzak@gsi.de>
2019-02-25 15:38:17 +0100

    Add documentation to KFMCCounter.

--- KFParticlePerformance/KFMCCounter.h
+++ KFParticlePerformance/KFMCCounter.h
@@ -16,19 +16,29 @@
 #include <fstream>
 #include <vector>
 
+/** @class KFMCCounter
+ ** @brief A helper structure to store information on the number of reconstructed and Monte Carlo particles for efficiency calculation.
+ ** @author  M.Zyzak, I.Kisel
+ ** @date 05.02.2019
+ ** @version 1.0
+ **
+ ** The class is used to calculate reconstruction efficiency and ratios of a given set of particles.
+ **/
+
 template <typename T>
-struct KFMCCounter // counters for different tracks categories
+struct KFMCCounter
 {
-  int NCounters;
+  int NCounters; ///< Number of counters in the current object.
   
-  std::vector<T> counters;
+  std::vector<T> counters; ///< Counters of different set of particles.
 
-  KFMCCounter():NCounters(0),counters(0) { };
-  KFMCCounter(int nCounters):NCounters(nCounters), counters(nCounters,T(0)) { };
+  KFMCCounter():NCounters(0),counters(0) { }
+  KFMCCounter(int nCounters):NCounters(nCounters), counters(nCounters,T(0)) { } ///< Constructs the object with the set of counters "nCounters".
 
-  void AddCounter(){ NCounters++; counters.push_back(T(0)); };
-  void AddCounters(int nCounters){ NCounters += nCounters; counters.resize( NCounters, T(0)); };
+  void AddCounter(){ NCounters++; counters.push_back(T(0)); } ///< Adds a counter to the existing list.
+  void AddCounters(int nCounters){ NCounters += nCounters; counters.resize( NCounters, T(0)); } ///< Adds several counters to the existing list.
   
+  /** Operator adds all counters from object "a" to the current object. Returns the current object. */
   KFMCCounter& operator+=(KFMCCounter& a){
     if (NCounters != a.NCounters){
       std::cout << " KFMCCounter: Error. Addition of counters of different sizes: " << NCounters << " " << a.NCounters << std::endl;
@@ -40,13 +50,13 @@ struct KFMCCounter // counters for different tracks categories
     }
     return *this;
   };
-
+  /** Operator adds all counters from object "a" to the current object, result is stored to the temporary object. Returns the temporary object. */
   KFMCCounter operator+(KFMCCounter& a){
     KFMCCounter res = *this;
     res += a;
     return res;
   };
-
+  /** Operator divides all counters from the current object to the counters from object "a", result is stored to the temporary object. Returns the temporary object. */
   template <typename T2>
   KFMCCounter<double> operator/(KFMCCounter<T2>& a){
     KFMCCounter<double> b(NCounters);
@@ -60,7 +70,7 @@ struct KFMCCounter // counters for different tracks categories
     }
     return b;
   }
-
+  /** Operator divides all counters from the current object to the value "a", result is stored to the temporary object. Returns the temporary object. */
   template <typename T2>
   KFMCCounter<T2> operator/(double a){
     KFMCCounter<T2> b(NCounters);
@@ -69,7 +79,7 @@ struct KFMCCounter // counters for different tracks categories
     }
     return b;
   }
-
+  /** Operator to write the object "a" to the file "strm".*/
   friend std::fstream & operator<<(std::fstream &strm, const KFMCCounter<T> &a ){
     strm << a.NCounters << " " << a.counters.size() << " ";
     for(unsigned int iV=0; iV<a.counters.size(); iV++)
@@ -77,7 +87,7 @@ struct KFMCCounter // counters for different tracks categories
     strm << std::endl;
     return strm;
   }
-
+  /** Operator to write the object "a" to the stream "strm".*/
   friend std::ostream & operator<<(std::ostream &strm, const KFMCCounter<T> &a ){
     strm << a.NCounters << " " << a.counters.size() << " ";
     for(unsigned int iV=0; iV<a.counters.size(); iV++)
@@ -85,7 +95,7 @@ struct KFMCCounter // counters for different tracks categories
     strm << std::endl;
     return strm;
   }
-
+  /** Operator to read the object "a" from the file "strm".*/
   friend std::fstream & operator>>(std::fstream &strm, KFMCCounter<T> &a ){
     int tmp;
     strm >> tmp;
@@ -102,7 +112,8 @@ struct KFMCCounter // counters for different tracks categories
   }
 
   private:
-    double Div(double a, double b){return (b > 0) ? a/b : -1.;};
+    /** Divides value "a" on value "b" if b is not zero, otherwise returns "-1". */
+    double Div(double a, double b){return (b > 0) ? a/b : -1.;}
 };
 
 #endif
